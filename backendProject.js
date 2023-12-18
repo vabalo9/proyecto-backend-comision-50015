d0946e22b81882299cc2d9264639ba949c3da84b
ValuPC <valentinabalo9@gmail.com>
2023-12-18 09:52:53 -0300

    segundo desafio parte 2

--- backendProject.js
+++ backendProject.js
@@ -1,11 +1,15 @@
+const fs= require("fs").promises
+
 
 class ProductManager {
     static ultId= 0;
-    constructor(){
+    constructor(path){
         this.products=[]
+        this.path=path
     }
 
-    addProduct(title,description,price,img,code,stock){
+    async addProduct(newObject){
+        let {title,description,price,img,code,stock}= newObject
         if (!title||!description||!price||!img||!code||!stock) {
             console.log("por favor complete todos los campos para poder ingresar el producto")
         }
@@ -17,26 +21,91 @@ class ProductManager {
         const newProduct={id:++ProductManager.ultId,title,description,price,img,code,stock}
 
         this.products.push(newProduct)
+
+        await this.guardarArchivo(this.products)
     }
 
     getProducts(){
         console.log(this.products)
     }
 
-    getProductById(id){
-        const product = this.product.find(product=>product.id==id)
-
-        if(!product){console.log("no se encontro el producto buscado")}
-        else{console.log("producto hayado")
-        return product}
+    async getProductById(id){
+        try {
+            const arrayProductos = await this.readArchive()
+            const buscado = arrayProductos.find(item=>item.id===id)
+            if(!buscado){
+                console.log("no se encontro ningún producto con el id mencionado")
+            }else{
+                return console.log(buscado)
+            }
+            
+        } catch (error) {
+            console.error("no se pudo leer el archivo")
+        }
         
+        
+        
+        
+        
+        
+    }
+
+    async readArchive(){
+        try {
+            const response = await fs.readFile(this.path, "utf-8")
+            const arrayProductos = JSON.parse(response)
+            return arrayProductos
+            
+        } catch (error) {
+            console.log("no se pudo leer correctamente el archivo")
+        }
+    }
+
+    async guardarArchivo(arrayProductos){
+        try {
+         await fs.writeFile(this.path, JSON.stringify(arrayProductos,null, 2))
+
+        } catch (error) {
+            console.log("error al guardar el archivo")
+        }
+    }
+
+    async updateProduct(id, actualicedProduct){
+        try {
+            const arrayProductos = await this.readArchive()
+            const index=arrayProductos.findIndex(item=>item.id===id)
+
+            if(index != -1){
+                arrayProductos.splice(index, 1 , actualicedProduct)
+                await this.guardarArchivo(arrayProductos)
+            }else{
+                console.log("no se encontro el producto")
+            }
+        } catch (error) {
+            console.error("no se pudo actualizar el producto")
+        }
+    }
+
+    async deleteProduct(id){
+        try {
+            const arrayProductos = await this.readArchive()
+            const cantidad = arrayProductos.length
+            let prueba = arrayProductos.filter(item=>item.id !== id)
+            console.log(prueba)
+            if (cantidad>prueba.length) {
+                console.log("producto eliminado exitosamente")
+            }else{
+                console.log("no se pudo eliminar el producto")
+            }
+            await this.guardarArchivo(prueba)
+            
+        } catch (error) {
+            console.error("inconveniente al momento de intentar eliminar el producto")
+        }
+
     }
 }
 
-//testing
 
-const manager = new ProductManager()
+const manager = new ProductManager("./productos.json")
 
-manager.addProduct("chaucha", "chaucha para comer saludablemente durante la semana", 50, "turl.png", 3312, 10000000)
-
-manager.getProducts()
